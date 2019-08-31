8143bd3fede80189efb1149ab4cb299f80a587b2
Tarun Aditya Thurlapati <tarun.aditya@hotmail.com>
2019-08-31 14:02:35 +0530

    Delete WRLDMaps.js

--- Website/wwwroot/WRLDMaps.js
+++
@@ -1,8 +0,0 @@
-//b7304a50c3e1bc3b5ab2b017f941f3c1238b7a952cb3c6ae8085abe225aa4cf23dd42cb3b4095124 is developer key
-//a1498ba0cc8f3defbc52710d95eb70df is my key
-
-var map = L.Wrld.map("WRLDMap", "b7304a50c3e1bc3b5ab2b017f941f3c1238b7a952cb3c6ae8085abe225aa4cf23dd42cb3b4095124",
-{
-    center: [37.7858, -122.401],
-    zoom: 16
-});
